c6d0bcce7e2fb537aee0bc4e3a813444a5e5a2eb
zaheen1234 <mohdzaheen2008@gmail.com>
2021-02-25 10:51:36 +0530

    added code for notch iPhoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -30,7 +30,8 @@ export class HomePage {
   }
 
   ngOnInit() {
-  
+    // alert('height : ' + this.platform.height());
+    // alert('width : ' + this.platform.width());
     this.lenOfQuestion = this.question.question.length;
     console.log('lenght of question : ', this.lenOfQuestion);
     if (this.lenOfQuestion > 70) {
